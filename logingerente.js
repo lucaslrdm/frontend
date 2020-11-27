0a363cf8124969a99eb6022147cbc078959bd8f8
lucaslrdm <74377755+lucaslrdm@users.noreply.github.com>
2020-11-27 19:28:43 -0300

    https://projeto-java-final.herokuapp.com

--- logingerente.js
+++ logingerente.js
@@ -41,7 +41,7 @@ function logar() {
         }
     }
 
-    fetch("https://backend-projetofinal1.herokuapp.com/"+flag, cabecalho)
+    fetch("https://projeto-java-final.herokuapp.com/"+flag, cabecalho)
         .then(res => res.json())
         .then(res => {
             localStorage.setItem("logado", JSON.stringify(res));
